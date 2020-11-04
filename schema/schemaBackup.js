e547a23ccf472a293975c01af2bd0e8a6a1de673
Sam <samblackburn2828@gmail.com>
2020-11-04 15:47:50 +0000

    mutations for users, reviews, delete and update

--- schema/schemaBackup.js
+++ schema/schemaBackup.js
@@ -1,9 +1,9 @@
-const graphql = require('graphql');
-const City = require('../models/cities');
-const Restaurant = require('../models/restaurants');
-const Review = require('../models/reviews');
-const User = require('../models/users');
-const Friend = require('../models/friends');
+const graphql = require("graphql");
+const City = require("../models/cities");
+const Restaurant = require("../models/restaurants");
+const Review = require("../models/reviews");
+const User = require("../models/users");
+const Friend = require("../models/friends");
 
 const {
   GraphQLObjectType,
@@ -11,32 +11,32 @@ const {
   GraphQLID,
   GraphQLList,
   GraphQLSchema,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLNonNull,
 } = graphql;
 
 // Creat City Schema
 
 const CityType = new GraphQLObjectType({
-  name: 'City',
+  name: "City",
   fields: () => ({
     id: {
-      type: GraphQLID
+      type: GraphQLID,
     },
     name: { type: GraphQLString },
     restaurants: {
       type: new GraphQLList(RestaurantType),
       resolve(parent, args) {
-        
         return Restaurant.find({ city_id: parent.id });
-      }
-    }
-  })
+      },
+    },
+  }),
 });
 
 //  Create User Schema
 
 const UserType = new GraphQLObjectType({
-  name: 'User',
+  name: "User",
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
@@ -46,48 +46,49 @@ const UserType = new GraphQLObjectType({
       type: new GraphQLList(ReviewType),
       resolve(parent, args) {
         return Review.find({ user_id: parent.id });
-      }
+      },
     },
     friends: {
       type: new GraphQLList(UserType),
       resolve(parent, args) {
-        
-        return User.find({ })
-      }
-    }
-  })
+        console.log(parent);
+        return User.find().where("_id").in(parent.friends);
+      },
+    },
+  }),
 });
 
 // Create Review Schema
 
 const ReviewType = new GraphQLObjectType({
-  name: 'Review',
+  name: "Review",
   fields: () => ({
     id: { type: GraphQLID },
     body: { type: GraphQLString },
     rating: { type: GraphQLInt },
+    img: { type: GraphQLString },
     restaurant_id: {
       type: RestaurantType,
       resolve(parent, args) {
         return Restaurant.findById(parent.restaurant_id);
-      }
+      },
     },
     user_id: {
       type: UserType,
       resolve(parent, args) {
         return User.findById(parent.user_id);
-      }
-    }
-  })
+      },
+    },
+  }),
 });
 
 // Create Restaurant Schema
 
 const RestaurantType = new GraphQLObjectType({
-  name: 'Restaurant',
+  name: "Restaurant",
   fields: () => ({
     id: {
-      type: GraphQLID
+      type: GraphQLID,
     },
     name: { type: GraphQLString },
     cuisine: { type: GraphQLString },
@@ -95,21 +96,21 @@ const RestaurantType = new GraphQLObjectType({
       type: CityType,
       resolve(parent, args) {
         return City.findById(parent.city_id);
-      }
+      },
     },
     reviews: {
       type: new GraphQLList(ReviewType),
       resolve(parent, args) {
         return Review.find({ restaurant_id: parent.id });
-      }
-    }
-  })
+      },
+    },
+  }),
 });
 
 // Create Query Schemas
 
 const RootQuery = new GraphQLObjectType({
-  name: 'RootQueryType',
+  name: "RootQueryType",
   fields: {
     // GET single City
     city: {
@@ -117,14 +118,14 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         return City.findById(args.id);
-      }
+      },
     },
     // Get all cities
     cities: {
       type: new GraphQLList(CityType),
       resolve(parent, args) {
         return City.find({});
-      }
+      },
     },
     // GET single restaurant
     restaurant: {
@@ -132,28 +133,28 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         return Restaurant.findById(args.id);
-      }
+      },
     },
     // GET all restaurants
     restaurants: {
       type: new GraphQLList(RestaurantType),
       resolve(parent, args) {
         return Restaurant.find({});
-      }
+      },
     },
     // GET all reviews
     reviews: {
       type: new GraphQLList(ReviewType),
       resolve(parent, args) {
         return Review.find({});
-      }
+      },
     },
     // GET all users
     users: {
       type: new GraphQLList(UserType),
       resolve(parent, args) {
         return User.find({});
-      }
+      },
     },
     // GET Single User
     user: {
@@ -161,53 +162,53 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         return User.findById(args.id);
-      }
-    }
-  }
+      },
+    },
+  },
 });
 
 // Create Mutation Schemas
 
 const Mutation = new GraphQLObjectType({
-  name: 'Mutation',
+  name: "Mutation",
   fields: {
     // City POST Request
     addCity: {
       type: CityType,
       args: {
         name: {
-          type: GraphQLString
-        }
+          type: GraphQLString,
+        },
       },
       resolve(parent, args) {
         let city = new City({
-          name: args.name
+          name: args.name,
         });
         return city.save();
-      }
+      },
     },
     // Restaurant POST request
     addRestaurant: {
       type: RestaurantType,
       args: {
         name: {
-          type: GraphQLString
+          type: GraphQLString,
         },
         cuisine: {
-          type: GraphQLString
+          type: GraphQLString,
         },
         city_id: {
-          type: GraphQLString
-        }
+          type: GraphQLString,
+        },
       },
       resolve(parent, args) {
         let restaurant = new Restaurant({
           name: args.name,
           cuisine: args.cuisine,
-          city_id: args.city_id
+          city_id: args.city_id,
         });
         return restaurant.save();
-      }
+      },
     },
     // User POST request
     addUser: {
@@ -215,37 +216,35 @@ const Mutation = new GraphQLObjectType({
       args: {
         name: { type: GraphQLString },
         avatarURL: { type: GraphQLString },
-        username: { type: GraphQLString}
+        username: { type: GraphQLString },
         // friends: { type: GraphQLString }
       },
       resolve(parent, args) {
         let user = new User({
           name: args.name,
           avatarURL: args.avatarURL,
-          username: args.username
+          username: args.username,
           // friends: args.friend_id
         });
         return user.save();
-      }
+      },
     },
     // PATCH Add friend to user
     addFriend: {
       type: UserType,
       args: {
         user_id: { type: GraphQLID },
-        friend_id: { type: GraphQLID }
+        friend_id: { type: GraphQLString },
       },
       resolve(parent, { user_id, friend_id }) {
-        return User.findById(friend_id).then((res) => {
-          return User.findByIdAndUpdate(
-            user_id,
-            {
-              $push: { friends: res }
-            },
-            { new: true }
-          )
-        })
-        }
+        return User.findByIdAndUpdate(
+          user_id,
+          {
+            $push: { friends: friend_id },
+          },
+          { new: true }
+        );
+      },
     },
     // Review POST request
     addReview: {
@@ -253,36 +252,40 @@ const Mutation = new GraphQLObjectType({
       args: {
         body: { type: GraphQLString },
         rating: { type: GraphQLInt },
+        img: { type: GraphQLString },
         restaurant_id: { type: GraphQLString },
-        user_id: { type: GraphQLString }
+        user_id: { type: GraphQLString },
       },
       resolve(parent, args) {
         let review = new Review({
           body: args.body,
           rating: args.rating,
+          img: args.img,
           restaurant_id: args.restaurant_id,
-          user_id: args.user_id
+          user_id: args.user_id,
         });
         return review.save();
-      }
+      },
     },
     // PATCH User
     updateUser: {
       type: UserType,
       args: {
-        user_id: { type: GraphQLID},
-        username: {type: GraphQLString},
-        avatarURL: {type: GraphQLString},
-        name: {type: GraphQLString}
+        user_id: { type: new GraphQLNonNull(GraphQLID) },
+        username: { type: GraphQLString },
+        avatarURL: { type: GraphQLString },
+        name: { type: GraphQLString },
       },
-      resolve(parent, {user_id, username, avatarURL, name}) {
-        const userToUpdate = User.findById(user_id)
+      resolve(parent, { user_id, username, avatarURL, name }) {
+        const userToUpdate = User.findById(user_id);
         if (username) {
-          userToUpdate.update({username: username})
-        } else if (avatarURL) {
-          userToUpdate.update(avatarURL)
-        } else if (name) {
-          userToUpdate.update(name)
+          userToUpdate.update({ username: username });
+        }
+        if (avatarURL) {
+          userToUpdate.update({ avatarURL: avatarURL });
+        }
+        if (name) {
+          userToUpdate.update({ name: name });
         }
         return userToUpdate;
         // const userToUpdate = User.findById(user_id);
@@ -291,15 +294,62 @@ const Mutation = new GraphQLObjectType({
         // });
         // const updatedUser = userToUpdate.save();
         // return updatedUser;
-      }
-    }
+      },
+    },
     // PATCH Review
+
+    updateReview: {
+      type: ReviewType,
+      args: {
+        review_id: { type: new GraphQLNonNull(GraphQLID) },
+        body: { type: GraphQLString },
+        rating: { type: GraphQLInt },
+        img: { type: GraphQLString },
+      },
+      resolve(parent, { review_id, body, rating, img }) {
+        const reviewToUpdate = Review.findById(review_id);
+        if (body) {
+          reviewToUpdate.update({ body: body });
+        }
+        if (rating) {
+          reviewToUpdate.update({ rating: rating });
+        }
+        if (img) {
+          reviewToUpdate.update({ img: img });
+        }
+        return reviewToUpdate;
+      },
+    },
+
     // DELETE User
+
+    deleteUser: {
+      type: UserType,
+      args: {
+        user_id: { type: new GraphQLNonNull(GraphQLID) },
+      },
+      resolve(parent, { user_id }) {
+        const deletedUser = User.findByIdAndRemove(user_id);
+        return deletedUser;
+      },
+    },
+
     // DELETE Review
-  }
+
+    deleteReview: {
+      type: ReviewType,
+      args: {
+        review_id: { type: new GraphQLNonNull(GraphQLID) },
+      },
+      resolve(parent, { review_id }) {
+        const deletedReview = Review.findByIdAndRemove(review_id);
+        return deletedReview;
+      },
+    },
+  },
 });
 
 module.exports = new GraphQLSchema({
   query: RootQuery,
-  mutation: Mutation
+  mutation: Mutation,
 });
